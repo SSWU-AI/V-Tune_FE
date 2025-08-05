f2b4b101d233cb06dd8b61313020eff531e7f18a
kseohyuz <seohyun518612@naver.com>
2025-08-05 14:57:52 +0900

    Chore: 백엔드 배포 주소 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api', // URL -> 배포 후 변경
+  baseURL: 'https://v-tune-be.onrender.com', // URL -> 백엔드 배포 주소
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
